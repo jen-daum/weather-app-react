8b28834523a33ba2b28e53bd57ea4124c235ac64
jen-daum <jennifer.daum.me@gmail.com>
2024-05-06 18:13:30 +0100

    weekly forecast is now pulling through properly

--- src/WeeklyWeatherInfo.js
+++ src/WeeklyWeatherInfo.js
@@ -1,20 +1,22 @@
 import React from "react";
 import moment from "moment";
 
-export default function WeeklyWeatherInfo({ props }) {
+export default function WeeklyWeatherInfo({ data }) {
   // Function to generate forecast items from forecast data
   function generateForecastItems() {
     let forecastItems = [];
-    if (!Array.isArray(props)) {
+    //console.log("Data length:", data.length);
+    if (!Array.isArray(data)) {
       // Check if data is definitely an array
+      //console.log("Data is not an array:", data);
       return forecastItems; // Return empty array if not
     }
-    for (let dayData of props) {
-      // Extract necessary properties
-      let { condition, time } = dayData;
+
+    for (let { condition, time } of data) {
+      //console.log("Condition:", condition);
+      //console.log("Time:", time);
       let date = moment(time * 1000).format("DD/MM");
       let day = moment(time * 1000).format("ddd");
-
       forecastItems.push(
         <div key={time} className="col-2">
           <ul>
